6c9fc0f7d03f92579c4d6ff4e8e22dd949cb3df7
Reza <Reza@DESKTOP-8465MUB>
2024-09-19 19:31:48 +0330

    first commit

---
+++ lib/common/utils/constants.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  
+  
+  static const dommain = 'https://app.rocket-soft.org';
+  static const baseUrl = '$dommain/api/development/';
+  static const apiKey = '1234';
+  static const scheme = 'academyapp';
+  
+  static final RouteObserver<ModalRoute<void>> singleCourseRouteObserver = RouteObserver<ModalRoute<void>>();
+  static final RouteObserver<ModalRoute<void>> contentRouteObserver = RouteObserver<ModalRoute<void>>();
+
+}
